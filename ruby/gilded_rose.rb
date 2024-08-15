e14a90d7352464ba2ad0e5d3b63c412a23ea0166
Aravind Arulmoli <aravind.arulmoli@card91.io>
2024-08-16 00:11:33 +0530

    Add comments

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,55 +12,40 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      next if item.name == SULFURAS; # Sulfuras is legendary with quality constant 80 (no update required)
+      # Sulfuras is legendary (quality remains constant at 80 - no update required))
+      next if item.name == SULFURAS;
 
       if item.name == AGED_BRIE
-        increase_quality(item, item.sell_in <= 0 ? 2 : 1)
+        # Aged Brie increases 2-fold if sell_in time is up, else increase 1-fold
+        amount_to_increase = item.sell_in <= 0 ? 2 : 1
+        increase_quality(item, amount_to_increase)
+
       elsif item.name == BACKSTAGE_PASSES
         if item.sell_in <= 0
+          # Backstage passes expire if sell_in time is up (concert is over)
           item.quality = 0
         else
-          increase_quality(item, item.sell_in <= 5 ? 3 : item.sell_in <= 10 ? 2 : 1)
+          # Backstage passes increase 3-fold if <= 5 days left, 2-fold if <= 10 day, 1-fold otherwise
+          amount_to_increase = item.sell_in <= 5 ? 3 : item.sell_in <= 10 ? 2 : 1;
+          increase_quality(item, amount_to_increase)
         end
+
       else
+        # Non-special items decrease 1-fold until sell_in time is up, then decrease 2-fold
         degrade_quality(item, item.sell_in <= 0 ? 2 : 1)
       end
 
-      # if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES
-      #   degrade_quality(item, 1)
-      # else
-      #   increase_quality(item, 1)
-      #   if item.name == BACKSTAGE_PASSES
-      #     if item.sell_in <= 10
-      #       increase_quality(item, 1)
-      #     end
-      #     if item.sell_in <= 5
-      #       increase_quality(item, 1)
-      #     end
-      #   end
-      # end
-
-      # if item.sell_in <= 0
-      #   if item.name != AGED_BRIE
-      #     if item.name != BACKSTAGE_PASSES
-      #       degrade_quality(item, 1)
-      #     else
-      #       item.quality = 0
-      #     end
-      #   else
-      #     increase_quality(item, 1)
-      #   end
-      # end
-
-      item.sell_in = item.sell_in - 1
+      item.sell_in -= 1
     end
   end
 
   def increase_quality(item, amount)
+    # Make sure quality dosen't exceed max quality allowed
     item.quality = [item.quality + amount, MAX_QUALITY].min
   end
 
   def degrade_quality(item, amount)
+    # Make sure quality dosen't go under min quality allowed
     item.quality = [item.quality - amount, MIN_QUALITY].max
   end
 end
