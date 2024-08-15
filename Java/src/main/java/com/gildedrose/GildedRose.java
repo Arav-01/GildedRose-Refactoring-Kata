d2457dba65ca6d637dd4115a6b3e26949b7b8089
Aravind Arulmoli <aravind.arulmoli@card91.io>
2024-08-15 18:31:41 +0530

    Refactor: extract method for incrementing and decrementing item quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,26 +17,19 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals(SULFURAS)) continue;
 
-            if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > MIN_QUALITY) {
-                    item.quality = item.quality - 1;
-                }
+            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+                decrementItemQuality(item);
             } else {
                 if (item.quality < MAX_QUALITY) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals(BACKSTAGE_PASSES)) {
                         if (item.sellIn < 11) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
+                            incrementItemQuality(item);
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
+                            incrementItemQuality(item);
                         }
                     }
                 }
@@ -47,18 +40,26 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (!item.name.equals(AGED_BRIE)) {
                     if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > MIN_QUALITY) {
-                            item.quality = item.quality - 1;
-                        }
+                        decrementItemQuality(item);
                     } else {
                         item.quality = 0;
                     }
                 } else {
-                    if (item.quality < MAX_QUALITY) {
-                        item.quality = item.quality + 1;
-                    }
+                    incrementItemQuality(item);
                 }
             }
+        }
+    }
+
+    private static void incrementItemQuality(Item item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality++;
+        }
+    }
+
+    private static void decrementItemQuality(Item item) {
+        if (item.quality > MIN_QUALITY) {
+            item.quality--;
         }
     }
 }
